df2c1113c9423dc9e612e7a35447c766067d6c65
yusefBoutahar <yusef_boutahar@hotmail.com>
2020-01-21 11:41:42 +0000

    refactoring

--- src/main/java/whoLikesIt.java
+++ src/main/java/whoLikesIt.java
@@ -1,8 +1,12 @@
 public class whoLikesIt {
 
     public static String whoLikesIt(String... names) {
-        if(names.length == 0) return "no one likes this";
+        if(names.length == 0){
+            return "no one likes this";
+        }else{
+            return "";
+        }
 
-        return "";
+
     }
 }
