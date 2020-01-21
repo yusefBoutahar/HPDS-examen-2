31b7729a4df8ec96e36b8fa11bdd3e57207e37f5
yusefBoutahar <yusef_boutahar@hotmail.com>
2020-01-21 12:43:31 +0000

    refactoring

--- src/main/java/whoLikesIt.java
+++ src/main/java/whoLikesIt.java
@@ -6,11 +6,6 @@ public class whoLikesIt {
         if(names.length == 2)return names[0] + " and " + names[1]+ " like this";
         if(names.length == 3)return names[0]+", "+names[1]+" and "+names[names.length-1]+" like this";
 
-
-        String res = "";
-        res += names[0]+", "+names[1]+" and "+ (names.length-2);
-
-        return res + " others like this";
-
+        return names[0] + ", " + names[1] + " and "+ (names.length-2) + " others like this";
     }
 }
