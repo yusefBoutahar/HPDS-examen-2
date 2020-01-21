a6b18f890cc266d2132f7f1adb045db584b9b28a
yusefBoutahar <yusef_boutahar@hotmail.com>
2020-01-21 12:27:23 +0000

    refactoring

--- src/main/java/whoLikesIt.java
+++ src/main/java/whoLikesIt.java
@@ -5,17 +5,18 @@ public class whoLikesIt {
         if(names.length == 1)return names[0]+" likes this";
         if(names.length == 2)return names[0] + " and " + names[1]+ " like this";
         if(names.length == 3)return names[0]+", "+names[1]+" and "+names[names.length-1]+" like this";
-        if(names.length >= 4)return names[0]+", "+names[1]+" and 2 others like this";
+        //if(names.length >= 4)return names[0]+", "+names[1]+" and 2 others like this";
+
+
 
                 //assertEquals("Alex, Jacob and 2 others like this", whoLikesIt.whoLikesIt("Alex", "Jacob", "Mark", "Max"));
-/*
+
         String res = "";
-        for (int i = 0; i < names.length-1 ; i++) {
-            res += names[i]+" and ";
+        for (int i = 0; i < 1 ; i++) {
+            res += names[i]+", ";
         }
-        res += names[names.length-1];
-        return res + " like this";
-*/
-        return "";
+        res += names[1] +" and 2"  ;
+        return res + " others like this";
+
     }
 }
