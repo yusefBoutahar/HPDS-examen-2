30cbd8d55b1d377b108e5ca4ef81b5c8e42de202
yusefBoutahar <yusef_boutahar@hotmail.com>
2020-01-21 12:16:10 +0000

    add testThreeLikes

--- src/main/java/whoLikesIt.java
+++ src/main/java/whoLikesIt.java
@@ -3,6 +3,7 @@ public class whoLikesIt {
     public static String whoLikesIt(String... names) {
         if(names.length == 0)return "no one likes this";
         if(names.length == 1)return names[names.length-1]+" likes this";
+        if(names.length == 3)return names[0]+", "+names[1]+" and "+names[names.length-1]+" like this";
 
         String res = "";
         for (int i = 0; i < names.length-1 ; i++) {
